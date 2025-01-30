0267cb81104465f0f6fcce6de6844ec86ae1e6c7
davidtakac <david.takac@mailbox.org>
2025-01-30 14:31:43 +0100

    1.7.0 (16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.davidtakac.bura"
         minSdk = 28
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.6.1"
+        versionCode = 16
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(
