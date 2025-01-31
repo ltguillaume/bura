c336f524e90f61e02bdf6a291b57ab95f2c4beb2
davidtakac <david.takac@mailbox.org>
2025-01-31 11:57:46 +0100

    Fix Compose ui tooling import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,8 +86,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
 }
